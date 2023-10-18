1485fedbfc363e56d6eba90740875548c0a998be
ViktorGetman <123113442+ViktorGetman@users.noreply.github.com>
2023-10-18 21:40:44 +0300

    Выполнено дз

--- AwesomeNetwork/Controllers/Account/AccountManagerController.cs
+++ AwesomeNetwork/Controllers/Account/AccountManagerController.cs
@@ -227,7 +227,11 @@ namespace AwesomeNetwork.Controllers.Account
 
             var result = await _userManager.GetUserAsync(currentuser);
 
-            var list = _userManager.Users.AsEnumerable().Where(x => x.GetFullName().ToLower().Contains(search.ToLower())).ToList();
+            var list = _userManager.Users.AsEnumerable().ToList();
+            if (!string.IsNullOrEmpty(search))
+            {
+                list = list.Where(x => x.GetFullName().ToLower().Contains(search.ToLower())).ToList();
+            }
             var withfriend = await GetAllFriend();
 
             var data = new List<UserWithFriendExt>();
